2963fce2e48ab830a01eaa004b1b00fe73042e52
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-24 18:54:06 -0600

    Componentes
    Se agregaron scripts C#

--- Proyecto curso/Assets/Scenes/Scripts/componente3.cs
+++ Proyecto curso/Assets/Scenes/Scripts/componente3.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Holamundo : MonoBehaviour
+public class componente3 : MonoBehaviour
 {
+    private void Awake()
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        print("algo paso");
-        Debug.Log("Algo paso");
-        Debug.LogWarning("Algo salio mal");
-        Debug.LogError("Algo salio muy mal");
+        componente1.miObjeto.name = "NuevoNombreDeObjeto";
     }
 
     // Update is called once per frame
