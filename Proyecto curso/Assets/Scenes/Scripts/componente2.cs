8d6dbb78563e7c710c8c8016080bb3b29753a989
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-24 19:37:57 -0600

    Tarea Practica modulo 5
    Se creo script C# donde se usan las funciones: LogError, LogWarning y Log de la clase debug para mostrar distintos tipos de Logs en la consola de Unity.

--- Proyecto curso/Assets/Scenes/Scripts/componente2.cs
+++ Proyecto curso/Assets/Scenes/Scripts/componente2.cs
@@ -6,17 +6,34 @@ public class componente2 : MonoBehaviour
 {
     private void Awake()
     {
-        
+        Debug.Log("Hola Mundo en void Awake");
+
     }
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(componente1.miObjeto.name);
+=======
+        Debug.Log("Hola Mundo en Void Start");
     }
+    // Start is called before the first frame update
+ 
+ 
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.LogWarning("Hola Mundo en Void Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogError("Hola Mundo en Void FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola Mundo en Void Late Update");
     }
 }
+
