e1b671c9f9dcd6392894571262be562d63bf0f25
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-25 18:59:41 -0600

    Arreglo problema
    Se arreglo problema con los scripts de componentes

--- Proyecto curso/Assets/Scenes/Scripts/Holav.cs
+++ Proyecto curso/Assets/Scenes/Scripts/Holav.cs
@@ -2,41 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Proyecto curso/Assets/Scenes/Scripts/Holav.cs
+
 public class Holav : MonoBehaviour
-========
+
 public class componente1 : MonoBehaviour
->>>>>>>> b580eb5 (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+
 {
-<<<<<<< HEAD:Proyecto curso/Assets/Scenes/Scripts/Hola mundo.cs
+
+   
+
     private void Awake()
     {
         Debug.Log("Hola en Awake");
+      
     }
-    // Start is called before the first frame update
+   
     void Start()
-<<<<<<<< HEAD:Proyecto curso/Assets/Scenes/Scripts/Holav.cs
+
     {
         Debug.Log("Hola en Start");
-========
-    { 
-        Debug.Log("Hola Mundo en Start");
-=======
-    public static GameObject miObjeto;
 
-    private void Awake()
-    {
-        miObjeto = this.gameObject;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-      
->>>>>>> 2963fce (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
->>>>>>>> b580eb5 (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
     }
 
-    // Update is called once per frame
     void Update()
     {
         Debug.Log("Hola en Update");
--- Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+++ Proyecto curso/Assets/Scenes/Scripts/componente1.cs
@@ -2,72 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Proyecto curso/Assets/Scenes/Scripts/Holav.cs
+
 public class Holav : MonoBehaviour
-========
+
 public class componente1 : MonoBehaviour
->>>>>>>> b580eb5 (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+
 {
-
-    private void Awake()
-    {
-        Debug.Log("Hola en Awake");
-    }
     // Start is called before the first frame update
-    void Start()
-<<<<<<<< HEAD:Proyecto curso/Assets/Scenes/Scripts/Holav.cs
-    {
-        Debug.Log("Hola en Start");
-========
-    { 
-        Debug.Log("Hola Mundo en Start");
-
->>>>>>> 8997288 (Edicion componente 1)
+ 
     public static GameObject miObjeto;
+
+     public static GameObject miObjeto;
+
 
     private void Awake()
     {
         miObjeto = this.gameObject;
-<<<<<<< Updated upstream
-=======
-        
->>>>>>> Stashed changes
+
     }
-    // Start is called before the first frame update
+   
     void Start()
     {
-<<<<<<< Updated upstream
-      
->>>>>>> 2963fce (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
->>>>>>>> b580eb5 (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Hola en Update");
-    }
-
-    private void FixedUpdate()
-    {
-        Debug.LogWarning("Hola en FixedUpdate");
-    }
-
-    private void LateUpdate()
-    {
-        Debug.LogError("Hola en LateUpdate");
-    }
-}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
-=======
-
-      
-    }
 }
 
->>>>>>> Stashed changes
->>>>>>> 8997288 (Edicion componente 1)
+
+
+
+
+      
