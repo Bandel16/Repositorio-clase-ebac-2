0990e66c3d712d65902b03cffbce2a593c823136
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-25 18:59:41 -0600

    Arreglo problema
    Se arreglo problema con los scripts de componentes

--- Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+++ Proyecto curso/Assets/Scenes/Scripts/componente1.cs
@@ -4,59 +4,23 @@ using UnityEngine;
 
 public class componente1 : MonoBehaviour
 {
-
-    private void Awake()
-    {
-        Debug.Log("Hola Mundo en Awake");
-    }
-    // Start is called before the first frame update
-    void Start()
-    { 
-        Debug.Log("Hola Mundo en Start");
-
->>>>>>> 8997288 (Edicion componente 1)
-    public static GameObject miObjeto;
+     public static GameObject miObjeto;
 
     private void Awake()
     {
         miObjeto = this.gameObject;
-<<<<<<< Updated upstream
-=======
-        
->>>>>>> Stashed changes
+
     }
-    // Start is called before the first frame update
+   
     void Start()
     {
-<<<<<<< Updated upstream
-      
->>>>>>> 2963fce (Componentes):Proyecto curso/Assets/Scenes/Scripts/componente1.cs
+
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        Debug.LogWarning("Hola Mundo en Update");
-    }
 
-    private void FixedUpdate()
-    {
-        Debug.LogError("Hola Mundo en FixedUpdate");
-    }
-
-    private void LateUpdate()
-    {
-        Debug.Log("Hola Mundo en Late Update");
     }
 }
-<<<<<<< HEAD
-
-=======
-=======
-
-      
-    }
-}
-
->>>>>>> Stashed changes
->>>>>>> 8997288 (Edicion componente 1)
+   
