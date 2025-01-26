1e160dc36160453108ed6e2ddcae15373b6344d0
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-25 18:55:11 -0600

    Hola Tarea
    Se creo script C#

---
+++ Proyecto curso/Assets/Scenes/Scripts/Holav.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holav : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Hola en Awake");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola en Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hola en Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola en FixedUpdate");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.LogError("Hola en LateUpdate");
+    }
+}
