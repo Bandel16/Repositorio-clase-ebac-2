1e5c2242ffac698a482690969db4971cf6b00e8f
Bandel16 <valdiviesojorgeangel@gmail.com>
2025-01-25 18:19:33 -0600

    Modificacion de archivo hola mundo
    Se modifico el scrpt c# de hola mundo

--- Proyecto curso/Assets/Scenes/Scripts/componente2.cs
+++ Proyecto curso/Assets/Scenes/Scripts/componente2.cs
@@ -6,7 +6,7 @@ public class componente2 : MonoBehaviour
 {
     private void Awake()
     {
-        Debug.Log("Hola Mundo en void Awake");
+        Debug.Log("Hola Mundo en Awake");
 
     }
     // Start is called before the first frame update
@@ -15,25 +15,27 @@ public class componente2 : MonoBehaviour
         Debug.Log(componente1.miObjeto.name);
 =======
         Debug.Log("Hola Mundo en Void Start");
+=======
+        
     }
-    // Start is called before the first frame update
+    
  
  
 
     // Update is called once per frame
     void Update()
     {
-        Debug.LogWarning("Hola Mundo en Void Update");
+        Debug.LogWarning("Hola Mundo en Update");
     }
 
     private void FixedUpdate()
     {
-        Debug.LogError("Hola Mundo en Void FixedUpdate");
+        Debug.LogError("Hola Mundo en FixedUpdate");
     }
 
     private void LateUpdate()
     {
-        Debug.Log("Hola Mundo en Void Late Update");
+        Debug.Log("Hola Mundo en Late Update");
     }
 }
 
